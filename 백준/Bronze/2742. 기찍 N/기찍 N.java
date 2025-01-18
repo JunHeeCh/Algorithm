24e5af3f8a35e7276437d129c3a2eefa849c89c1
JunHeeCh <apdlsid1@naver.com>
2025-01-18 22:50:00 +0900

    [Bronze IV] Title: 기찍 N, Time: 840 ms, Memory: 33208 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        for(int b=a;b>=1;b--){
+            System.out.println(b);
+        }
+        
+    }
+}
