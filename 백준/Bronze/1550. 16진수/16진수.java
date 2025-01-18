afd2af870186d510be7dabc364ce1dcfdbdb6dc5
JunHeeCh <apdlsid1@naver.com>
2025-01-18 22:12:19 +0900

    [Bronze II] Title: 16진수, Time: 172 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
+        int b = Integer.parseInt(a,16);
+        System.out.print(b);
+        
+    }
+}
