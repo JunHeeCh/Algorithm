1a71d0bce0421ca655aa8f3ec7901b9f5713800b
JunHeeCh <apdlsid1@naver.com>
2025-01-19 13:43:27 +0900

    [Bronze IV] Title: Hello Judge, Time: 204 ms, Memory: 18028 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        for(int b = 1 ; b<=a ; b++){
+            System.out.printf("Hello World, Judge %d!\n", b);
+        }
+    }
+}
