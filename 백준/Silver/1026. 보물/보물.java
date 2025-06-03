dce93b3816944397e3d2efc5949a55a9abd94d17
JunHeeCh <apdlsid1@naver.com>
2025-06-03 19:25:09 +0900

    [Silver IV] Title: 보물, Time: 188 ms, Memory: 17832 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] A = new int[N];
+		int[] B = new int[N];
+
+		for (int i = 0; i < A.length; i++) {
+			A[i] = sc.nextInt();
+		}
+
+		for (int i = 0; i < B.length; i++) {
+			B[i] = sc.nextInt();
+		}
+
+		Arrays.sort(A);
+		Arrays.sort(B);
+		
+		int sum = 0;
+		
+		for (int i = 0; i < B.length; i++) {
+			sum += A[i]*B[N-1-i];
+		}
+		
+		System.out.println(sum);
+	}
+
+
+}
