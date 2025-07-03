a73e257096c070fbb9665ed168706ae2262e66a5
JunHeeCh <apdlsid1@naver.com>
2025-07-03 21:51:36 +0900

    [Gold V] Title: CCW, Time: 168 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Gold/11758. CCW/CCW.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // 세 점의 좌표 입력
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+        int x3 = sc.nextInt();
+        int y3 = sc.nextInt();
+
+        long cross = (long)(x2 - x1) * (y3 - y1) - (long)(y2 - y1) * (x3 - x1);
+        
+        if (cross > 0) {
+            System.out.println(1);  // 반시계방향
+        } else if (cross < 0) {
+            System.out.println(-1); // 시계방향
+        } else {
+            System.out.println(0);  // 일직선
+        }
+        
+        sc.close();
+    }
+}
