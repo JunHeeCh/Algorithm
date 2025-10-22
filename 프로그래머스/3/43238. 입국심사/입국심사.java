398c38cfee3aa4ff4acb880978d6f42b46bb9df0
JunHeeCh <apdlsid1@naver.com>
2025-10-22 22:40:09 +0900

    [level 3] Title: 입국심사, Time: 142.04 ms, Memory: 89.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+        
+        Arrays.sort(times);
+        
+        long left = 1;
+        long right = (long) times[times.length-1]*n;
+        answer = right;
+        
+        while(left <= right){
+            long mid = (left+right)/2;
+            
+            long count = 0;
+            for(int time : times){
+                count += mid/time;
+                if(count >= n) break;
+            }
+            
+            if(count >= n){
+                answer = mid;
+                right = mid-1;
+            }else{
+                left = mid+1;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
