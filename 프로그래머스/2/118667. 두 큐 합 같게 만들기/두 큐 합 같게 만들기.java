e44666874efbb1a9d6cc078a94ad6c08f6305198
JunHeeCh <apdlsid1@naver.com>
2026-01-28 20:37:38 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 19.17 ms, Memory: 140 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int solution(int[] queue1, int[] queue2) {
+        long sum1 = 0, sum2 = 0;
+        for (int x : queue1) sum1 += x;
+        for (int x : queue2) sum2 += x;
+
+        long total = sum1 + sum2;
+        if (total % 2 != 0) return -1;
+        long target = total / 2;
+        
+        int n = queue1.length;
+        int[] arr = new int[n*2];
+        
+        for(int i=0; i<n; i++){
+            arr[i] = queue1[i];
+            arr[i+n] = queue2[i];
+        }
+        
+        int left = 0;
+        int right = n;
+        long cur = sum1;
+        int count = 0;
+        
+        while(left < n*2 && right < n*2){
+            if(cur == target) return count;
+            
+            if(cur < target){
+                cur += arr[right++];
+            }else{
+                cur -= arr[left++];
+            }
+
+            count++;
+        }
+        return -1;
+    }
+}
