6a47a71684e7f96b4fa8407d8124d1ffdabf89f7
JunHeeCh <apdlsid1@naver.com>
2026-02-18 20:23:15 +0900

    [level 2] Title: 요격 시스템, Time: 397.42 ms, Memory: 170 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] targets) {
+        int answer = 0;
+        
+        Arrays.sort(targets, (a,b)->{
+            return Integer.compare(a[1], b[1]);
+        });
+        
+        int last = -1;
+        
+        for(int i=0; i<targets.length; i++){
+            if(last <= targets[i][0]){
+                answer++;
+                last = targets[i][1];
+            }
+            
+        }
+            
+        return answer;
+    }
+}
