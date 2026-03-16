5a6d9a63b064b19779c97447195afd5e7757d456
JunHeeCh <apdlsid1@naver.com>
2026-03-16 21:59:02 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/README.md
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 조건에 맞는 사용자 정보 조회하기 - 164670 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164670) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164670?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2025년 12월 22일 22:32:41
+2026년 03월 16일 21:58:59
 
 ### 문제 설명
 
--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,11 +1,11 @@
 -- 코드를 입력하세요
-SELECT USER_ID, NICKNAME, CONCAT(CITY," ",STREET_ADDRESS1," ",STREET_ADDRESS2) AS 전체주소, CONCAT(SUBSTRING(TLNO,1,3),"-",SUBSTRING(TLNO,4,4),"-",SUBSTRING(TLNO,8,4)) AS 전화번호
-FROM USED_GOODS_USER 
-WHERE USER_ID IN (
-    SELECT WRITER_ID
-    FROM USED_GOODS_BOARD
-    GROUP BY WRITER_ID
-    HAVING COUNT(BOARD_ID)>=3
-)
-ORDER BY USER_ID DESC
 
+select USER_ID, nickname, CITY || ' ' || STREET_ADDRESS1 || ' ' || STREET_ADDRESS2 전체주소, substr(tlno, 1, 3) || '-' || substr(tlno, 4, 4) || '-' || substr(tlno, 8, 4) 전화번호
+from USED_GOODS_USER 
+where USER_ID in (
+    SELECT WRITER_ID
+    FROM USED_GOODS_BOARD 
+    GROUP BY WRITER_ID
+    HAVING COUNT(BOARD_ID) >= 3
+)
+order by user_id desc
