0d58739760ce671e476c0cd450d01b0355684c6d
JunHeeCh <apdlsid1@naver.com>
2026-04-06 14:38:14 +0900

    [Silver V] Title: 다리 놓기, Time: 132 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -2,37 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	static long[][] dp = new long[31][31];
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int T = Integer.parseInt(br.readLine().trim());
 		StringTokenizer st;
 
-		while (T-- > 0) {
+		while (T > 0) {
 			st = new StringTokenizer(br.readLine().trim());
 
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 
-			get(a, b);
+			long ans = 1;
 
-			System.out.println(dp[b][a]);
+			for (int i = 0; i < a; i++) {
+				ans *= (b - i);
+				ans /= (i + 1);
+			}
+
+			System.out.println(ans);
+			T--;
 		}
 
-	}
-
-	private static long get(int a, int b) {
-		if (a == 0 || a == b) {
-			return dp[b][a] = 1;
-		}
-
-		if (dp[b][a] == 0) {
-			dp[b][a] = get(a, b - 1) + get(a - 1, b - 1);
-		}
-		return dp[b][a];
 	}
 
 }
