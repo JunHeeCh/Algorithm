1e949ae9fc189185f09c56c4ae6a87a1dc06826e
JunHeeCh <apdlsid1@naver.com>
2026-06-07 21:23:24 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 43.48 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,66 +1,65 @@
 import java.util.*;
 
 class Solution {
-    Map<Integer, Integer> map = new HashMap(); // 번호, 석유 크기 
-    int idx;
+
     int[] dr = new int[]{-1,1,0,0};
     int[] dc = new int[]{0,0,-1,1};
+    int[] arr;
     
     public int solution(int[][] land) { // 1이면 석유
         int answer = 0;
         
-        idx = -1;
+        // 1. 우선 bfs로 각 땅 별 석유의 크기를 계산한다 
+        // 1-1. 이때, 가장 왼쪽 땅 위치와 오른쪽 땅 위치를 가지고, 해당 땅의 위치에 석유 크기를 저장한다 
+        int n = land.length; 
+        int m = land[0].length;
+        arr = new int[m];
         
-        boolean[][] visited = new boolean[land.length][land[0].length];
-        
-        for(int i=0; i<land.length; i++){
-            for(int j=0; j<land[0].length; j++){
-                if(land[i][j] == 1 && !visited[i][j]){
+        boolean[][] visited = new boolean[n][m];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(land[i][j]==1 && !visited[i][j]){
                     bfs(land, visited, i, j);
-                    idx--;
                 }
             }
         }
         
-        for(int j=0; j<land[0].length; j++){
-            int total = 0;
-            Set<Integer> set = new HashSet();
-            
-            for(int i=0; i<land.length; i++){
-                if(land[i][j]<0 && !set.contains(land[i][j])){
-                    total += map.get(land[i][j]);
-                    set.add(land[i][j]);
-                }
-            }
-            
-            answer = Math.max(answer, total);
+        // 2. 가로 길이를 돌면서 최대값을 찾는다 
+        for(int i=0; i<m; i++){
+            answer = Math.max(answer, arr[i]);
         }
         return answer;
     }
     
     public void bfs(int[][] land, boolean[][] visited, int r, int c){
+        int[] now = new int[]{r,c};
         Queue<int[]> queue = new LinkedList();
-        queue.add(new int[]{r,c});
+        queue.add(now);
         visited[r][c] = true;
-        land[r][c] = idx;
         
-        int count = 1;
-        
+        int size = 1;
+        int left = c;
+        int right = c;
         while(!queue.isEmpty()){
-            int[] now = queue.poll();
+            now = queue.poll();
+            
             for(int i=0; i<dr.length; i++){
                 int nr = now[0]+dr[i];
                 int nc = now[1]+dc[i];
-                if(nr < 0 || nc < 0 || nr >= land.length || nc >= land[0].length || visited[nr][nc]) continue;
-                if(land[nr][nc]==1){
-                    queue.add(new int[]{nr,nc});
-                    visited[nr][nc] = true;
-                    land[nr][nc] = idx;
-                    count++;
-                }
+                
+                if(nr<0 || nr >=land.length || nc <0 || nc >= land[0].length || land[nr][nc] == 0 || visited[nr][nc]) continue;
+                visited[nr][nc] = true;
+                queue.add(new int[]{nr,nc});
+                size++;
+                left = Math.min(left, nc);
+                right = Math.max(right, nc);
             }
         }
-        map.put(idx, count);
-        return;
+        
+        for(int i=left; i<=right; i++){
+            arr[i] += size;
+        }
+        
     }
+    
 }
