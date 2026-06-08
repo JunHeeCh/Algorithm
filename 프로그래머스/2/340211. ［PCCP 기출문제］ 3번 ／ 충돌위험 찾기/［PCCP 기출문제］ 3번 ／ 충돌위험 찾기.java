9964be5285b686296762de737ecfa36a2a5f7537
JunHeeCh <apdlsid1@naver.com>
2026-06-08 19:01:54 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 118.92 ms, Memory: 308 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,48 +1,80 @@
 import java.util.*;
 
 class Solution {
+
+    int answer = 0;
+
     public int solution(int[][] points, int[][] routes) {
-        int answer = 0;
-        
-        // key: "시간,r,c" → 해당 시간/위치에 있는 로봇 수
-        Map<String, Integer> map = new HashMap<>();
-        
-        for (int i = 0; i < routes.length; i++) {
-            int r = points[routes[i][0] - 1][0];
-            int c = points[routes[i][0] - 1][1];
-            int t = 0;
-            
-            // 시작 위치 기록
-            String key = t + "," + r + "," + c;
-            map.put(key, map.getOrDefault(key, 0) + 1);
-            
-            // 경유지 순서대로 이동
-            for (int j = 1; j < routes[i].length; j++) {
-                int tr = points[routes[i][j] - 1][0];
-                int tc = points[routes[i][j] - 1][1];
-                
-                // 행 먼저 이동
-                while (r != tr) {
-                    r += (r < tr) ? 1 : -1;
-                    t++;
-                    key = t + "," + r + "," + c;
-                    map.put(key, map.getOrDefault(key, 0) + 1);
+        List<Node> bots = new ArrayList<>();
+
+        for (int[] route : routes) {
+            int[][] waypoints = new int[route.length][];
+            for (int i = 0; i < route.length; i++) {
+                waypoints[i] = points[route[i] - 1];
+            }
+            bots.add(new Node(waypoints));
+        }
+
+        while (bots.stream().anyMatch(b -> !b.fin)) {
+            check(bots);
+            move(bots);
+        }
+
+        return answer;
+    }
+
+    public void check(List<Node> bots) {
+        int[][] visited = new int[101][101];
+
+        for (Node n : bots) {
+            if (n.fin) continue; // fin된 봇은 이미 필드에 없음
+            int r = n.now[0], c = n.now[1];
+            visited[r][c]++;
+            if (visited[r][c] == 2) {
+                answer++;
+            }
+        }
+    }
+
+    public void move(List<Node> bots) {
+        for (Node n : bots) {
+            if (n.fin) continue;
+
+            int[] goal = n.waypoints[n.routeIdx];
+
+            if (n.now[0] != goal[0]) {
+                n.now[0] += (n.now[0] < goal[0]) ? 1 : -1;
+            } else if (n.now[1] != goal[1]) {
+                n.now[1] += (n.now[1] < goal[1]) ? 1 : -1;
+            } else {
+                // 경유지 도달
+                n.routeIdx++;
+                if (n.routeIdx >= n.waypoints.length) {
+                    n.fin = true; // 다음 check()부터 제외됨
+                    continue;
                 }
-                // 열 이동
-                while (c != tc) {
-                    c += (c < tc) ? 1 : -1;
-                    t++;
-                    key = t + "," + r + "," + c;
-                    map.put(key, map.getOrDefault(key, 0) + 1);
+                // 다음 경유지로 이동
+                goal = n.waypoints[n.routeIdx];
+                if (n.now[0] != goal[0]) {
+                    n.now[0] += (n.now[0] < goal[0]) ? 1 : -1;
+                } else {
+                    n.now[1] += (n.now[1] < goal[1]) ? 1 : -1;
                 }
             }
         }
-        
-        // 2대 이상 겹친 위치(시간 포함) 개수 카운트
-        for (int val : map.values()) {
-            if (val >= 2) answer++;
-        }
-        
-        return answer;
+    }
+}
+
+class Node {
+    int[] now;
+    int[][] waypoints;
+    int routeIdx;
+    boolean fin;
+
+    public Node(int[][] waypoints) {
+        this.waypoints = waypoints;
+        this.now = waypoints[0].clone();
+        this.routeIdx = 1; // 향하는 목적지는 1번부터
+        this.fin = (waypoints.length == 1); // 경유지가 시작점 하나뿐이면 즉시 fin
     }
 }
