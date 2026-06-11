10bb218e729ded99ca360ccdbf85a5e403c6f286
JunHeeCh <apdlsid1@naver.com>
2026-06-11 20:30:08 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수레 움직이기, Time: 2.06 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.java
+++ 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.java
@@ -1,95 +1,82 @@
 import java.util.*;
 
 class Solution {
-    
-    int[] dr = new int[]{-1,1,0,0,0};
-    int[] dc = new int[]{0,0,-1,1,0};
-    
+    int[] dr = {-1, 1, 0, 0};
+    int[] dc = {0, 0, -1, 1};
+    int n, m;
+    int[][] maze;
+    int[] redE, blueE;
+    int answer = Integer.MAX_VALUE;
+
     public int solution(int[][] maze) {
-        /*
-        <조건>
-        1. 방문했던 곳 이동 불가 
-        2. 도착 시 해당 위치 고정 
-        3. 같은 위치에 수레 이동 불가 
-        => 최소한의 턴 수를 반환하라 
-        풀 수 없는 경우 0 반환 
+        this.maze = maze;
+        n = maze.length;
+        m = maze[0].length;
 
-        */
-        
-        int n = maze.length; 
-        int m = maze[0].length;
-        
-        int[] redS = null, redE = null, blueS = null, blueE = null; 
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(maze[i][j]==1){
-                    redS = new int[]{i,j};
-                }else if(maze[i][j]==2){
-                    blueS = new int[]{i,j};
-                }else if(maze[i][j]==3){
-                    redE = new int[]{i,j};
-                }else if(maze[i][j]==4){
-                    blueE = new int[]{i,j};
-                }
+        int[] redS = null, blueS = null;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (maze[i][j] == 1) redS = new int[]{i, j};
+                else if (maze[i][j] == 2) blueS = new int[]{i, j};
+                else if (maze[i][j] == 3) redE = new int[]{i, j};
+                else if (maze[i][j] == 4) blueE = new int[]{i, j};
             }
         }
 
-        Set<String> visited = new HashSet();
-        Queue<int[]> queue = new ArrayDeque();
-        
-        int redStartBit = 1 << (m * redS[0] + redS[1]);
-        int blueStartBit = 1 << (m * blueS[0] + blueS[1]);
+        boolean[][] rv = new boolean[n][m];
+        boolean[][] bv = new boolean[n][m];
+        rv[redS[0]][redS[1]] = true;
+        bv[blueS[0]][blueS[1]] = true;
 
-        queue.add(new int[]{redS[0], redS[1],blueS[0], blueS[1],redStartBit,blueStartBit,0});
-
-        visited.add(redS[0]+","+redS[1]+","+blueS[0]+","+blueS[1]+","+redStartBit+","+blueStartBit);
-
-        
-        while(!queue.isEmpty()){
-            int[] now = queue.poll();
-            
-            boolean flagR = false;
-            boolean flagB = false;
-            
-            if(now[0]==redE[0]&&now[1]==redE[1]) flagR = true;
-            if(now[2]==blueE[0]&&now[3]==blueE[1]) flagB = true;
-            
-            if(flagR && flagB){
-                return now[6];
-            }
-            
-            for(int i=0; i<4; i++){
-                if(flagR) i=4;
-                for(int j=0; j<4; j++){
-                    if(flagB) j=4;
-                    int Rr = now[0]+dr[i];
-                    int Rc = now[1]+dc[i];
-                    int Br = now[2]+dr[j];
-                    int Bc = now[3]+dc[j];
-                    
-                    if(Rr<0||Rr>=n||Rc<0||Rc>=m||Br<0||Br>=n||Bc<0||Bc>=m|| maze[Rr][Rc]==5 || maze[Br][Bc]==5 || (Rr==Br&&Rc==Bc) || (Rr==now[2] && Rc==now[3] && Br==now[0] && Bc==now[1])) continue;
-
-                    int bitR = m*Rr+Rc;
-                    int bitB = m*Br+Bc;
-                    if(i!=4 && (now[4]&(1<<bitR))!=0) continue;  
-                    if(j!=4 && (now[5]&(1<<bitB))!=0) continue;
-                    
-                    int maskR = now[4] | (1<<bitR);
-                    int maskB = now[5] | (1<<bitB);
-                    
-                    String key = Rr+","+Rc+","+Br+","+Bc+","+maskR+","+maskB;
-                    
-                    if(visited.contains(key)) continue;
-                    queue.add(new int[]{Rr,Rc,Br,Bc,maskR, maskB, now[6]+1});
-                    visited.add(key);
-
-                }
-            }
-        }
-        
-        return 0;
+        dfs(redS[0], redS[1], blueS[0], blueS[1], 0, rv, bv);
+        return answer == Integer.MAX_VALUE ? 0 : answer;
     }
-    
-    
+
+    void dfs(int rr, int rc, int br, int bc, int turn, boolean[][] rv, boolean[][] bv) {
+        if (turn >= answer) return; // 가지치기
+
+        boolean rDone = (rr == redE[0] && rc == redE[1]);
+        boolean bDone = (br == blueE[0] && bc == blueE[1]);
+        if (rDone && bDone) { answer = Math.min(answer, turn); return; }
+
+        // 도착한 수레는 제자리(이동 안 함), 아직이면 반드시 한 칸 이동
+        List<int[]> redMoves = buildMoves(rr, rc, rDone, rv);
+        List<int[]> blueMoves = buildMoves(br, bc, bDone, bv);
+
+        for (int[] rm : redMoves) {
+            for (int[] bm : blueMoves) {
+                // 같은 칸 금지
+                if (rm[0] == bm[0] && rm[1] == bm[1]) continue;
+                // 자리 맞바꿈(swap) 금지
+                if (rm[0] == br && rm[1] == bc && bm[0] == rr && bm[1] == rc) continue;
+
+                boolean redMoved = !(rm[0] == rr && rm[1] == rc);
+                boolean blueMoved = !(bm[0] == br && bm[1] == bc);
+
+                if (redMoved) rv[rm[0]][rm[1]] = true;
+                if (blueMoved) bv[bm[0]][bm[1]] = true;
+
+                dfs(rm[0], rm[1], bm[0], bm[1], turn + 1, rv, bv);
+
+                if (redMoved) rv[rm[0]][rm[1]] = false;   // 백트래킹
+                if (blueMoved) bv[bm[0]][bm[1]] = false;
+            }
+        }
+    }
+
+    List<int[]> buildMoves(int r, int c, boolean done, boolean[][] visited) {
+        List<int[]> moves = new ArrayList<>();
+        if (done) {            // 도착했으면 그 자리에 고정
+            moves.add(new int[]{r, c});
+            return moves;
+        }
+        for (int i = 0; i < 4; i++) {   // 아직이면 4방향으로 반드시 이동
+            int x = r + dr[i], y = c + dc[i];
+            if (x < 0 || x >= n || y < 0 || y >= m) continue;
+            if (maze[x][y] == 5) continue;       // 벽
+            if (visited[x][y]) continue;          // 이미 지난 칸 금지
+            moves.add(new int[]{x, y});
+        }
+        return moves;
+    }
 }
