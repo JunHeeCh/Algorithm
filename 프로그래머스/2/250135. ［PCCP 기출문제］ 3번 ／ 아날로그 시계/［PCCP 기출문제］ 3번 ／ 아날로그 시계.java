37ab15b9afe716e86774bf87f012cb411522ffae
JunHeeCh <apdlsid1@naver.com>
2026-06-12 22:04:52 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 아날로그 시계, Time: 9.01 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.java
+++ 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.java
@@ -1,46 +1,53 @@
 class Solution {
+    double h = 1.0/120;
+    double m = 1.0/10;
+    double s = 6.0;
+
     public int solution(int h1, int m1, int s1, int h2, int m2, int s2) {
         int answer = 0;
-        
-        // 초침이 시침 or 분침과 겹치는 횟수를 반환하라 
-        // 각각 구해서 더하기 
-        // 동시에 겹치는 경우가 있다면 제거 
-        
-        // 시침은 60*60*12초 에 360도를 회전
-        // 분침은 60*60초 에 360도를 회전
-        double hour = 0.1/12; // 초당 회전 
-        double min = 0.1; 
-        double sec = 6;
-        
         int start = cal(h1,m1,s1);
         int end = cal(h2,m2,s2);
-        
-        double nowH = (hour * start) % 360;
-        double nowM = (min  * start) % 360;
-        double nowS = (sec  * start) % 360;
-        
-        if(nowH==nowS) answer++;
-        if(nowM==nowS) answer++;
-        
-        for(int i=start+1; i<=end; i++){
-            double prevDiffH = sec*(i-1) - hour*(i-1);
-            double currDiffH = sec*i     - hour*i;
-            if (Math.floor(currDiffH/360) > Math.floor(prevDiffH/360)) answer++;
 
-            double prevDiffM = sec*(i-1) - min*(i-1);
-            double currDiffM = sec*i     - min*i;
-            if (Math.floor(currDiffM/360) > Math.floor(prevDiffM/360)) answer++;
+        degree prev = calDegree(start);
+        for(int i=start+1; i<=end; i++){
+            degree now = calDegree(i);
+            answer += plus(prev.hour, now.hour, prev.sec, now.sec);
+            answer += plus(prev.min,  now.min,  prev.sec, now.sec);
+            prev = now;
         }
-        
-        if(start==0) answer--;
-        if(start <= cal(12,0,0) && end >= cal(12,0,0)) answer--;
+
+        degree st = calDegree(start);
+        if(st.sec == st.hour) answer++;
+        if(st.sec == st.min)  answer++;
+
+        for(int i=start; i<=end; i++){
+            degree d = calDegree(i);
+            if(d.sec == d.hour && d.hour == d.min) answer--;
+        }
         return answer;
     }
+
+    public int plus(double start, double end, double secP, double secN){
+    double rel = 6 - (end - start);
+    double gap = ((start - secP) % 360 + 360) % 360;
+    return (gap > 1e-9 && gap <= rel + 1e-9) ? 1 : 0;
+}
+
+    public degree calDegree(int time){
+        double hour = (time*h)%360;
+        double min  = (time*m)%360;
+        double sec  = (time*s)%360;
+        return new degree(hour, min, sec);
+    }
     
-    public int cal(int h, int m, int s){
-        int sum = h*60*60;
-        sum += m*60;
-        sum += s;
-        return sum;
+    public int cal(int h, int m, int s){ 
+        return h*3600 + m*60 + s;
+    }
+}
+
+class degree{
+    double hour, min, sec;
+    public degree(double hour, double min, double sec){
+        this.hour=hour; this.min=min; this.sec=sec;
     }
 }
