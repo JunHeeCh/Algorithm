5bad7766405739f12e8d04b316fba316f775bb02
JunHeeCh <apdlsid1@naver.com>
2026-07-22 14:46:43 +0900

    [level 4] Title: 징검다리, Time: 21.19 ms, Memory: 89.8 MB -BaekjoonHub

---
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int distance, int[] rocks, int n) {
+        int answer = 0;
+        
+        Arrays.sort(rocks);
+        
+        int left = 1;
+        int right = distance;
+        
+        while(left <= right){
+            int mid = (left+right)/2;
+            
+            int res = cal(mid, rocks, distance);
+            
+            if(res > n){
+                right = mid-1;
+            }else{
+                answer = mid;
+                left = mid+1;
+            }
+        }
+        
+        return answer; 
+    }
+    
+    public int cal(int dist, int[] rocks, int distance){
+        int prev = 0;
+        int removed = 0;
+        for(int i=0; i<rocks.length; i++){
+            if(rocks[i]-prev < dist){
+                removed++;
+            }else{
+                prev = rocks[i];
+            }
+        }
+        if(distance-prev < dist){
+            removed++;
+        }
+        return removed;
+    }
+}
