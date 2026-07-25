70528186c7b50600701563d9db2a00d3099e244a
JunHeeCh <apdlsid1@naver.com>
2026-07-25 17:54:49 +0900

    [level 1] Title: 과일 장수, Time: 30.33 ms, Memory: 121 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        
+        int len = score.length;
+        
+        Arrays.sort(score);
+        
+        int cnt = 0;
+        for(int i=len-1; i>=0; i--){
+            cnt++;
+            if(cnt==m){
+                answer += m*score[i];
+                cnt = 0;
+            }
+        }
+        
+        return answer;
+    }
+}
