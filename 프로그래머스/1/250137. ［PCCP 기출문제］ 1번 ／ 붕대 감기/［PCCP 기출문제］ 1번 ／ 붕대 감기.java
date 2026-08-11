de6585da12aea92eb3247b6c6b9a96762d00a35a
JunHeeCh <apdlsid1@naver.com>
2026-08-11 11:17:37 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.07 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -1,43 +1,26 @@
-/*
-t초 동안 붕대를 감으면 1초마다 x만큼 회복
-t초 연속 성공 시 y만큼 추가 회복 
 
-
-*/
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
-        
-        int time = bandage[0]; // 시전 시간
-        int recovery = bandage[1]; // 초당 회복량
-        int plusRecovery = bandage[2]; // 추가 회복량 
-        
+        // 1초마다 x 만큼 회복
+        // t초 회복 시 y 만큼 추가 회복 
+        int time = 0;
         int max = health;
         
-        // health는 초기 체력 및 최대 체력
-        
-        health -= attacks[0][1]; // 처음 공격을 맞음 
-        
-        for(int i=1; i<attacks.length; i++){
-            if(health<=0) {
-                health = -1;
-                break;
-            }
-            int attackTime = attacks[i][0];
-            int damage = attacks[i][1];
+        for(int i=0; i<attacks.length; i++){
+            int t = attacks[i][0]-time-1;
+            time = attacks[i][0];
+
+            int moc = t/bandage[0];
+            health += t*bandage[1];
+            health += moc*bandage[2];
+            health = Math.min(health, max);
             
-            // 1. 이전 시간과 지금 시간동안 추가한 체력을 보충
-            int diff = attacks[i][0]-attacks[i-1][0]-1;
-            health += recovery*diff;
-            diff /= time;
-            health += diff*plusRecovery;
-            if(health>max){
-                health = max;
+            health -= attacks[i][1];
+            if(health<=0){
+                return -1;
             }
-            // 2. 공격받은 만큼 체력 감소 
-            health -= damage;
         }
-        
-        if(health<=0) return -1;
+
         return health;
     }
 }
