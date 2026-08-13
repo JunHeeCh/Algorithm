f0efb361fd3420672177cb77ea289ef46d51c2de
JunHeeCh <apdlsid1@naver.com>
2026-08-13 13:25:11 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 25.60 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -4,28 +4,34 @@ class Solution {
 
     public int solution(int[] diffs, int[] times, long limit) {
         
-        int left = 1, right = 100000;
+        int left = 1;
+        int right = 100000;
         
         while(left < right){
             int mid = (left+right)/2;
-            if(cal(diffs,times,mid)>limit){
+            long res = cal(diffs, times, mid);
+            if(res > limit){
                 left = mid+1;
             }else{
                 right = mid;
             }
         }
-
         return left;
     }
     
     public long cal(int[] diffs, int[] times, int level){
-        long total = Math.max(0, diffs[0]-level)*(times[0])+times[0];
-        
-        for(int i=1; i<diffs.length; i++){
-            total += Math.max(0, diffs[i]-level)*(times[i]+times[i-1])+times[i];    
+        long res = 0;
+        for(int i=0; i<diffs.length; i++){
+            if(diffs[i]>level){
+                if(i>0){
+                    res += (times[i]+times[i-1])*(diffs[i]-level);
+                }else{
+                    res += (times[i])*(diffs[i]-level);
+                }
+            }
+            res += times[i];
         }
-        
-        return total;
+        return res;
     }
 
 }
