571fc35791f782802216b1ed316a80f639279e21
JunHeeCh <apdlsid1@naver.com>
2026-08-14 10:54:15 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 54.76 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -4,62 +4,54 @@ class Solution {
 
     int[] dr = new int[]{-1,1,0,0};
     int[] dc = new int[]{0,0,-1,1};
-    int[] arr;
+    int[] oil;
     
-    public int solution(int[][] land) { // 1이면 석유
+    public int solution(int[][] land) { 
         int answer = 0;
         
-        // 1. 우선 bfs로 각 땅 별 석유의 크기를 계산한다 
-        // 1-1. 이때, 가장 왼쪽 땅 위치와 오른쪽 땅 위치를 가지고, 해당 땅의 위치에 석유 크기를 저장한다 
-        int n = land.length; 
-        int m = land[0].length;
-        arr = new int[m];
+        boolean[][] visited = new boolean[land.length][land[0].length];
+        oil = new int[land[0].length];
         
-        boolean[][] visited = new boolean[n][m];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+        for(int i=0; i<land.length; i++){
+            for(int j=0; j<land[0].length; j++){
                 if(land[i][j]==1 && !visited[i][j]){
+                    visited[i][j] = true;
                     bfs(land, visited, i, j);
                 }
             }
         }
         
-        // 2. 가로 길이를 돌면서 최대값을 찾는다 
-        for(int i=0; i<m; i++){
-            answer = Math.max(answer, arr[i]);
+        for(int i=0; i<oil.length; i++){
+            answer = Math.max(answer, oil[i]);
         }
         return answer;
     }
     
     public void bfs(int[][] land, boolean[][] visited, int r, int c){
-        int[] now = new int[]{r,c};
-        Queue<int[]> queue = new LinkedList();
-        queue.add(now);
-        visited[r][c] = true;
+        Set<Integer> set = new HashSet();
+        set.add(c);
+        int territory = 1;
         
-        int size = 1;
-        int left = c;
-        int right = c;
+        Queue<int[]> queue = new ArrayDeque();
+        queue.add(new int[]{r,c});
+        
         while(!queue.isEmpty()){
-            now = queue.poll();
+            int[] now = queue.poll();
             
-            for(int i=0; i<dr.length; i++){
+            for(int i=0; i<4; i++){
                 int nr = now[0]+dr[i];
                 int nc = now[1]+dc[i];
-                
-                if(nr<0 || nr >=land.length || nc <0 || nc >= land[0].length || land[nr][nc] == 0 || visited[nr][nc]) continue;
-                visited[nr][nc] = true;
+                if(nr < 0 || nc < 0 || nr >= land.length || nc >= land[0].length || land[nr][nc]==0 || visited[nr][nc]) continue;
                 queue.add(new int[]{nr,nc});
-                size++;
-                left = Math.min(left, nc);
-                right = Math.max(right, nc);
+                territory++;
+                visited[nr][nc] = true;
+                set.add(nc);
             }
         }
         
-        for(int i=left; i<=right; i++){
-            arr[i] += size;
+        for(int i:set){
+            oil[i] += territory;
         }
-        
     }
     
 }
