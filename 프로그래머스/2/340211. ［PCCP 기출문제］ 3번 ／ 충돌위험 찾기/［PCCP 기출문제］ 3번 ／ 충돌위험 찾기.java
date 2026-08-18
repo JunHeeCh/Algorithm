14ac763da6bce24b1d11529ff497f029738adf2c
JunHeeCh <apdlsid1@naver.com>
2026-08-18 11:47:07 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 120.44 ms, Memory: 306 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -2,79 +2,75 @@ import java.util.*;
 
 class Solution {
 
-    int answer = 0;
-
     public int solution(int[][] points, int[][] routes) {
-        List<Node> bots = new ArrayList<>();
+        int answer = 0;
 
-        for (int[] route : routes) {
-            int[][] waypoints = new int[route.length][];
-            for (int i = 0; i < route.length; i++) {
-                waypoints[i] = points[route[i] - 1];
-            }
-            bots.add(new Node(waypoints));
+        List<node> list = new ArrayList<>();
+        for (int i = 0; i < routes.length; i++) {
+            int[] start = points[routes[i][0] - 1].clone();
+            list.add(new node(start, routes[i]));
         }
 
-        while (bots.stream().anyMatch(b -> !b.fin)) {
-            check(bots);
-            move(bots);
-        }
+        answer += check(list);
 
+        while (list.size() > 0) {
+            move(list, points);
+            answer += check(list);
+        }
         return answer;
     }
 
-    public void check(List<Node> bots) {
-        int[][] visited = new int[101][101];
+    public void move(List<node> list, int[][] points) {
+        for (int i = 0; i < list.size(); i++) {
+            node cur = list.get(i);
+            int[] target = points[cur.route[cur.idx] - 1];
 
-        for (Node n : bots) {
-            if (n.fin) continue; // fin된 봇은 이미 필드에 없음
-            int r = n.now[0], c = n.now[1];
-            visited[r][c]++;
-            if (visited[r][c] == 2) {
-                answer++;
+            if (cur.now[0] == target[0] && cur.now[1] == target[1]) {
+                cur.idx++;
+                if (cur.idx == cur.route.length) {
+                    list.remove(i);
+                    i--;
+                    continue;
+                }
+                target = points[cur.route[cur.idx] - 1];
+            }
+            if (cur.now[0] != target[0]) {
+                if (cur.now[0] > target[0]) {
+                    cur.now[0]--;
+                } else {
+                    cur.now[0]++;
+                }
+            } else {
+                if (cur.now[1] > target[1]) {
+                    cur.now[1]--;
+                } else {
+                    cur.now[1]++;
+                }
             }
         }
     }
 
-    public void move(List<Node> bots) {
-        for (Node n : bots) {
-            if (n.fin) continue;
-
-            int[] goal = n.waypoints[n.routeIdx];
-
-            if (n.now[0] != goal[0]) {
-                n.now[0] += (n.now[0] < goal[0]) ? 1 : -1;
-            } else if (n.now[1] != goal[1]) {
-                n.now[1] += (n.now[1] < goal[1]) ? 1 : -1;
-            } else {
-                // 경유지 도달
-                n.routeIdx++;
-                if (n.routeIdx >= n.waypoints.length) {
-                    n.fin = true; // 다음 check()부터 제외됨
-                    continue;
-                }
-                // 다음 경유지로 이동
-                goal = n.waypoints[n.routeIdx];
-                if (n.now[0] != goal[0]) {
-                    n.now[0] += (n.now[0] < goal[0]) ? 1 : -1;
-                } else {
-                    n.now[1] += (n.now[1] < goal[1]) ? 1 : -1;
-                }
+    public int check(List<node> list) {
+        int answer = 0;
+        int[][] board = new int[101][101];
+        for (node n : list) {
+            if (board[n.now[0]][n.now[1]] == 1) {
+                answer++;
             }
+            board[n.now[0]][n.now[1]]++;
         }
+        return answer;
     }
 }
 
-class Node {
+class node {
     int[] now;
-    int[][] waypoints;
-    int routeIdx;
-    boolean fin;
+    int[] route;
+    int idx;
 
-    public Node(int[][] waypoints) {
-        this.waypoints = waypoints;
-        this.now = waypoints[0].clone();
-        this.routeIdx = 1; // 향하는 목적지는 1번부터
-        this.fin = (waypoints.length == 1); // 경유지가 시작점 하나뿐이면 즉시 fin
+    public node(int[] now, int[] route) {
+        this.now = now;
+        this.route = route;
+        this.idx = 1;   
     }
 }
