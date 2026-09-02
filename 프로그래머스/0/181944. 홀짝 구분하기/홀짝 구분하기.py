3746d70ebd0dcb32f71e68c3469bbe6badf236ae
JunHeeCh <apdlsid1@naver.com>
2026-09-02 10:53:48 +0900

    [level 0] Title: 홀짝 구분하기, Time: 13.46 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.py
@@ -0,0 +1,11 @@
+# a = int(input())
+# if (a%2==0):
+#     print(str(a)+ " is even")
+# else:
+#     print (str(a)+ " is odd")
+    
+a = input()
+if(int(a)%2==0):
+    print(a+" is even")
+else:
+    print(a+" is odd")
